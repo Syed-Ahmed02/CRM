cb131d3b70ac73328ce1af5a43d41a4fc85988fe
Syed-Ahmed02 <syedahmedd.02@gmail.com>
2026-05-02 16:20:06 -0400

    Set up ConvexDB

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
-import { Geist, Geist_Mono, Inter, Nunito_Sans } from "next/font/google"
+import { Geist_Mono, Inter, Nunito_Sans } from "next/font/google"
 
 import "./globals.css"
+import { ConvexClientProvider } from "@/components/convex-client-provider"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-const nunitoSansHeading = Nunito_Sans({subsets:['latin'],variable:'--font-heading'});
+const nunitoSansHeading = Nunito_Sans({
+  subsets: ["latin"],
+  variable: "--font-heading",
+})
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -22,10 +26,17 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable, nunitoSansHeading.variable)}
+      className={cn(
+        "font-sans antialiased",
+        fontMono.variable,
+        inter.variable,
+        nunitoSansHeading.variable,
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   )
